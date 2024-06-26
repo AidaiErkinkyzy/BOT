35369b98ef39a2082e1b2aba5327d7248de4a6db
Erkin kyzy Aidai <a73613821@gmail.com>
2024-06-26 17:23:42 +0600

    2nd commit

--- handlers/commands.py
+++ handlers/commands.py
@@ -20,6 +20,7 @@ async def start(message: Message):
         [KeyboardButton(text="/Facebook")],
         [KeyboardButton(text="/бизнес_клуб_Атланты")],  
         [KeyboardButton(text="/Ostavit_zayavku")],
+        [KeyboardButton(text="/add_client")],
         [KeyboardButton(text="/end")]
     ]
     keyboard = ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
@@ -46,12 +47,12 @@ async def send_1_picture(callback:CallbackQuery):
     await callback.message.answer_photo(
         photo=FSInputFile("1.jpg"),
         caption = ( # Описание к фото
-            "Телеграм https://t.me/+996997777733 \n"
-            "Инстаграм - https://www.instagram.com/proconsultkg/ \n"
-            "Тел: 12:30–13:30 \n"
-            "График работы Пн-Пт 08:30–17:30,Обед 12:30–13:30, Выходной Субб-Вс \n"
-            "Адрес - Токтогула, 147, 2 этаж, 40/1 офис, Первомайский район, Бишкек, 720001 \n"
-            "Ссылка на сайт - https://finjust.kg/ru/ \n"
+            "Телеграм: https://t.me/ \n"
+            "Инстаграм: https://www.instagram.com/proconsultkg/ \n"
+            "Тел: +996997777733 \n"
+            "График работы: Пн-Пт 08:30–17:30,Обед 12:30–13:30, Выходной Субб-Вс \n"
+            "Адрес: Токтогула, 147, 2 этаж, 40/1 офис, Первомайский район, Бишкек, 720001 \n"
+            "Ссылка на сайт: https://finjust.kg/ru/ \n"
         )
     )
 
@@ -76,55 +77,13 @@ async def бизнес_клуб_Атланты(message:Message):
     )
 
 
+
+
 # Обработчик для команды /end
 @router.message(Command(commands=['end']))
 async def end(message: Message):
     await message.answer("Спасибо, что воспользовались helpfindwork_bot! До новых встреч!", 
                         show_alert=True)
-
-
-
-
-
-
-
-
-
-# @router.message(Command(commands=['Ostavit_zayavku']))
-# async def Ostavit_zayavku(message:Message):
-#     await message.answer()
-
-
-# @router.message(Command(commands=['Ostavit_zayavku']))
-# async def Ostavit_zayavku(message:Message):
-#     CONTACTS = (
-#         "Мы - ПроКонсалт \n"
-#     )
-
-#     keyboard = InlineKeyboardMarkup(inline_keyboard=[
-#         [InlineKeyboardButton(text="Ostavit_zayavku", callback_data="1")],
-#         ])
-
-#     await message.answer(CONTACTS,reply_markup=keyboard)
-
-
-
-# @router.callback_query(F.data=="1")
-# async def send_1_picture(callback:CallbackQuery):
-#     await callback.message.answer_photo(
-#         photo=FSInputFile("1.jpg"),
-#         caption = ( # Описание к фото
-#             "Телеграм https://t.me/+996997777733 \n"
-#             "Инстаграм - https://www.instagram.com/proconsultkg/ \n"
-#             "Тел: 12:30–13:30 \n"
-#             "График работы Пн-Пт 08:30–17:30,Обед 12:30–13:30, Выходной Субб-Вс \n"
-#             "Адрес - Токтогула, 147, 2 этаж, 40/1 офис, Первомайский район, Бишкек, 720001 \n"
-#             "Ссылка на сайт - https://finjust.kg/ru/ \n"
-#         )
-#     )
-
-
-
 
 
 
@@ -146,8 +105,9 @@ bot_instance: Bot = None  # Глобальная переменная для х�
 
 class Form(StatesGroup):
     name = State()  # Состояние для ФИО
-    phone_number = State()  # Состояние для номера телефона
     age = State()
+    phone_number = State()  # Состояние для номера телефона
+    chat_id = State()
     experience = State()
 
 def setup_routers(dispatcher: Dispatcher, bot: Bot):
@@ -156,14 +116,23 @@ def setup_routers(dispatcher: Dispatcher, bot: Bot):
     dispatcher.include_router(router)
 
 
-@router.message(Command("ostavit_zayavku"))
+@router.message(Command("Ostavit_zayavku"))
 async def cmd_ostavit_zayavku(message: Message, state: FSMContext):
     await message.answer("Пожалуйста, введите ваше ФИО:")
     await state.set_state(Form.name)
 
+
 @router.message(Form.name)
 async def process_name(message: Message, state: FSMContext):
     await state.update_data(name=message.text)
+    await message.answer("Теперь введите ваш возраст:")
+    await state.set_state(Form.age)
+
+
+
+@router.message(Form.age)
+async def process_age(message: Message, state: FSMContext):
+    await state.update_data(age=message.text)
     await message.answer("Теперь введите ваш номер телефона:")
     await state.set_state(Form.phone_number)
 
@@ -171,13 +140,13 @@ async def process_name(message: Message, state: FSMContext):
 @router.message(Form.phone_number)
 async def process_phone_number(message: Message, state: FSMContext):
     await state.update_data(phone_number=message.text)
-    await message.answer("Теперь введите ваш возраст:")
-    await state.set_state(Form.age)
+    await message.answer("Теперь введите ваш chat_id:")
+    await state.set_state(Form.chat_id)
 
 
-@router.message(Form.age)
-async def process_age(message: Message, state: FSMContext):
-    await state.update_data(age=message.text)
+@router.message(Form.chat_id)
+async def process_chat_id(message: Message, state: FSMContext):
+    await state.update_data(chat_id=message.text)
     await message.answer("Опыт работы:")
     await state.set_state(Form.experience)
 
@@ -187,23 +156,112 @@ async def process_experience(message: Message, state: FSMContext):
     await state.set_state(Form.experience)
 
 
-@router.message(Form.experience)
-async def process_experience(message: Message, state: FSMContext):
-    await state.update_data(experience=message.text)
-    data = await state.get_data()
+
+
+
+
+
+
 
 
     # Отправляем сообщение пользователю
     await message.answer(
-        f"Записаны!"
+        f"Заявка принята! \n"
+        # f"ФИО: {data['name']}\n"
+        # f"Номер телефона: {data['phone_number']}\n"
+        # f"Chat_id: {data["chat_id"]}\n"
+        # f"Опыт работы:{data["experience"]}"
     )
-    
+
+
+
     # Отправляем сообщение администратору
     user = message.from_user
     admin_message = (
-        f"Новый клиент! Позвоните, для потверждения записи на номер +996 558-39-86-21! \n"
+        f"Новый клиент! Позвоните, для потверждения заявки, на номер +996997777733! \n"
     )
     # await message.answer(ADMIN_CHAT_ID, admin_message)
     await bot_instance.send_message(ADMIN_CHAT_ID, admin_message)
 
     await state.clear()
+
+
+
+
+
+
+
+
+
+
+
+
+
+from aiogram.types import Message,CallbackQuery,FSInputFile,ReplyKeyboardRemove
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+
+class AddClientStates(StatesGroup):
+    first_name = State()
+    last_name = State()
+    age = State()
+    phone = State()
+    chat_id = State()
+    experience = State()
+
+
+
+@router.message(Command("add_client"))
+async def cmd_add_client(message:Message,state: FSMContext):
+    await state.set_state(AddClientStates.first_name)
+    await message.answer("Enter first_name:")
+    
+
+@router.message(AddClientStates.first_name)
+async def process_first_name(message: Message, state: FSMContext) -> None:
+    await state.update_data(first_name=message.text)
+    await state.set_state(AddClientStates.last_name)
+    await message.answer("Enter last_name:")
+
+
+@router.message(AddClientStates.last_name)
+async def process_last_name(message: Message, state: FSMContext) -> None:
+    await state.update_data(last_name=message.text)
+    await state.set_state(AddClientStates.age)
+    await message.answer("Enter age:")
+
+
+@router.message(AddClientStates.age)
+async def process_age(message: Message, state: FSMContext) -> None:
+    await state.update_data(age=message.text)
+    await state.set_state(AddClientStates.phone)
+    await message.answer("Enter phone:")
+
+@router.message(AddClientStates.phone)
+async def process_phone(message: Message, state: FSMContext) -> None:
+    await state.update_data(phone=int(message.text))
+    await state.set_state(AddClientStates.chat_id)
+    await message.answer("Enter chat_id:")
+
+@router.message(AddClientStates.chat_id)
+async def process_chat_id(message: Message, state: FSMContext) -> None:
+    await state.update_data(chat_id=message.text)
+    await state.set_state(AddClientStates.experience)
+    await message.answer("Enter experience:")
+
+@router.message(AddClientStates.experience)
+async def process_experience(message: Message, state: FSMContext) -> None:
+    await state.update_data(experience=int(message.text))
+    data = await state.get_data()
+    await show_summary(message, data)
+    await state.clear()
+
+async def show_summary(message: Message, data: dict[str, any]):
+    client = Client(first_name=data["first_name"],
+            last_name=data["last_name"],
+            age=data["age"],
+            phone=data["phone"],
+            chat_id=data["chat_id"], 
+            experience=data["experience"])
+    result = await create_client(client)
+    await message.answer(text="Успешно добавили клиента!", reply_markup=ReplyKeyboardRemove())
